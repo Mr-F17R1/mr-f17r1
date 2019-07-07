e7a2bb7bc19a706a9573febc18e7ceca162d5025
Mr-F17R1 <52620990+Mr-F17R1@users.noreply.github.com>
2019-07-07 16:33:01 +0700

    Add files via upload

---
+++ Bucin.py
@@ -0,0 +1,93 @@
+clear
+
+figlet Tool | lolcat
+figlet Bucin | lolcat
+figlet By | lolcat
+figlet MR.F17R1| lolcat
+echo "================================================
+# by      : FIKRI
+# Special : BUCIN
+================================================" | lolcat
+sleep 1
+echo "[*] Hay sayang " | lolcat
+sleep 1
+echo "[*] maaf aku ga beri apa apa " | lolcat
+sleep 1
+echo "[*] HADIA DARI MR.F17R1 BUAT BUCIN " | lolcat
+sleep 1
+echo "[!] KLICK Y! (Y/N) : $hadiah" | lolcat
+read hadiah
+if [ $hadiah = y ] || [ $hadiah = Y ]
+then
+clear
+figlet Formulir | lolcat
+figlet Sederhana | lolcat
+echo "[#] Masukan Nama anda : $nama" | lolcat
+read nama
+echo "[#] Masukan Usia anda Sekarang : $usia" | lolcat
+read usia
+echo "[#] Masukan Hobi kamu : $hobi" | lolcat
+read hobi
+echo "[#] Sebutkan Cita Cita kamu : $cita" | lolcat
+read cita
+echo "[#] Sebutkan Nama Pacar kamu : $pacar" | lolcat
+read pacar
+echo "[#] Masukan Pesan Yang ingin kamu sampaikan ke pacar kamu : $pesan" | lolcat
+read pesan
+echo "[#] Masukan Harapanmu : $harapan" | lolcat
+read harapan
+echo "[#] Masukan pesan Hal yang paling kamu benci dari hubungan : $benci" | lolcat
+read benci
+echo "[#] Masukan Tanggal lahir Pacar kamu : $tanggal" | lolcat
+read tanggal
+echo "[#] Masukan Tanggal Jadian kalian : $january" | lolcat
+read january
+echo "[*] Data sedang di Proses..." | lolcat
+sleep 3
+clear
+fi
+if [ $hadiah = n ] || [ $hadiah = N ]
+then
+clear
+figlet Hehe | lolcat
+echo "Makase lh gak mukak" | lolcat
+sleep 1
+exit
+fi
+sleep 1
+toilet -f standard -F gay "HBD syg"
+sleep 1
+echo "[*] Buka Hadiah? (Y/N) : $hehe"| lolcat
+read hehe
+if [ $hehe = y ] || [ $hehe = Y ]
+then
+echo "[#] Selamat ultah $nama yang ke $usia " | lolcat
+sleep 1
+echo "[#] Harapan kamu $harapan Adalah harapan ku Juga" | lolcat
+sleep 1
+echo "[#] Cita cita kamu $cita Semoga tercapaikan dan jadi kenyataan " | lolcat
+sleep 1
+echo "[#] Pacar kamu $pacar, pesan kamu untuk dia $pesan" | lolcat
+sleep 1
+echo "[#] Hal yang kamu tidak ingin dari dia $benci " | lolcat
+sleep 1
+echo "[#] Hoby mu $hobi adalah kebangganku" | lolcat
+sleep 1
+echo "[#] Tanggal lahir pacar mu $tanggal, Dan yang pasti tanggal jadian kalian $january " | lolcat
+sleep 1
+echo "[+] Maaf ok yank Terimalah aku apa adanya :') " | lolcat
+echo "[=] klik Y : $Y" | lolcat
+read Y
+if [ $Y = Y ] || [ $Y = y ]
+then
+figlet I LOVE YOU | lolcat
+fi
+fi
+if [ $hehe = n ] || [ $hehe = N ]
+then
+clear
+figlet Hehe | lolcat
+echo "LOVE YOU DEA" | lolcat
+sleep 1
+exit
+fi
